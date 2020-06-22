dba5e6c3fa853f854dfbe30d272ca3d3b97077fd
Ubuntu <ubuntu@ip-172-31-94-12.ec2.internal>
2020-06-22 21:52:14 +0000

    Commit with manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    private final String hello;
+    public WelcomeController ( @Value("${welcome.message}") String message) {
+         this.hello= message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+         return hello;
+    }
 }
